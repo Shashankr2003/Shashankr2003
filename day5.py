ab81a56587e8d507e9ee8070c255be387c246fe2
Shashank R <shankrofficial123@gmail.com>
2026-07-24 07:43:38 +0530

    Reverse and sort a list of numbers
    Sort the list in descending order and then reverse it.

---
+++ day5.py
@@ -0,0 +1,11 @@
+#%%
+#Reverse and Sort a List: Create a list of numbers and:
+#Sort it in descending order.
+#Reverse the sorted list and print it.
+a = [1,5,16,4,25,45,87]
+
+print(sorted(a))
+a.reverse()
+print(a)
+
+
